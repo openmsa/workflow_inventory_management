04f00cc3fe5d5f94f52e6bc3a642370fcf49070e
dma <dma@ubiqube.com>
2022-05-06 18:19:12 +0100

    wf - alignment with inventory DA update

--- VLAN_POOL_MGNT/Process_Get_New_VLAN_Id/Tasks/Task_Get_existing_VLAN_Pools.py
+++ VLAN_POOL_MGNT/Process_Get_New_VLAN_Id/Tasks/Task_Get_existing_VLAN_Pools.py
@@ -41,7 +41,7 @@ devicelongid = device_id[3:]
 #{"Gateway":"0"}
 #micro_service_vars_array = {"object_id":object_id}
 object_parameters = {}
-object_parameters['VLAN_POOL'] ={}
+object_parameters['VLAN_POOL'] = 0
 
 
 # call the CREATE for the specified MS for each device
